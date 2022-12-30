a89e23a9d96b730a551cb9414ac186a66e045a55
apcoding1 <121611443+apcoding1@users.noreply.github.com>
2022-12-30 17:46:25 +0530

    commit-1

---
+++ Assignment_1.java
@@ -0,0 +1,29 @@
+class Assignment_1 {
+    public static void main(String[] args) {
+        // Q.1
+            int x =2;
+            int y = 4;
+            System.out.println("Q.1-The Product of "+x+" and "+y+ " is "+ x*y);
+            //Q.2
+            char character;
+            String input="U";
+            character = input.charAt(0) ;
+            int x1 = character; 
+            System.out.println("Q.2-The ASCII value of  "+ character +" is "+x1);
+            //Q.3
+            int length =7;
+            int breadth = 4;
+            System.out.println("Q.3-The area of the rectangle is "+ length*breadth);
+            //Q.4
+            int num=4;
+            System.out.println("Q.4-The cube of "+ num +" is "+ (num)*(num)*(num));
+            //Q.5
+            int x3 = 2;
+            int y3 = 3;
+            int thrvar=y3;
+            y3 = x3;
+            x3 = thrvar;
+            System.out.println("Q.5-Output(swap(2,3))- "+x3+","+y3);
+            
+    }
+}
